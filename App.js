53bbbc0a0e413166777b5d1b90176b889970b586
magda <midzik@consol.pl>
2017-07-07 12:27:10 +0200

    Fajny search

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, Button, Picker } from 'react-native';
 import axios from 'axios';
 import _ from 'lodash';
 
@@ -14,10 +14,13 @@ export default class App extends React.Component {
         zipcode : ZIP_CODE,
         days : [],
         forecast: {},
-        text: "I'm reterded."
+        text: "I'm reterded.",
+        city: "12566",
+        cities: [{"name": "b"}, {"name": "124"}, {"name" : "aaa"}]
     }
     this._getForecast = this._getForecast.bind(this)
     this._onPressMagicButton = this._onPressMagicButton.bind(this)
+    this._onPressMagicButton2 = this._onPressMagicButton2.bind(this)
 
   }
   _getForecast(zipcode){
@@ -36,7 +39,21 @@ export default class App extends React.Component {
   }
 
   _onPressMagicButton() {
-    this._getForecast(this.state.text)
+    this._getForecast(this.state.city)
+  }
+
+  _onPressMagicButton2() {
+    const cities_url = "http://autocomplete.wunderground.com/aq?query="+this.state.text
+
+    axios.get(cities_url).then( (response)=> {
+        if(response.status == 200){
+           /* var weather = response.data.forecast.simpleforecast.forecastday;
+            var forecast = [];*/
+            console.log(response.data);
+            this.setState({cities: _.get(response.data, 'RESULTS')});
+        }
+
+    });
   }
 
   render() {
@@ -52,6 +69,25 @@ export default class App extends React.Component {
           onChangeText={(text) => this.setState({text})}
           value={this.state.text}
         />
+
+        <Button
+          onPress={this._onPressMagicButton2}
+          title="POKA MI MIASTA"
+          color="#841584"
+        ></Button>
+
+
+        <Picker
+          style={{height: 100, width: 300}}
+          selectedValue={this.state.city}
+          onValueChange={(itemValue, itemIndex) => this.setState({city: itemValue})}>
+          {this.state.cities.map(function(object, i){
+            return <Picker.Item label={object['name']} value={object['l']} />;
+          })}
+
+
+        </Picker>
+
         <Button
           onPress={this._onPressMagicButton}
           title="POKA MI POGODE"
