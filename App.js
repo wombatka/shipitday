74671ca4380d9a91a27a192c0d7d4d038a69a8f2
Joanna Niechwiedowicz <joannan@consol.de>
2017-07-07 10:51:01 +0200

    basic requesst

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import axios from 'axios'
-
+import axios from 'axios';
+const ZIP_CODE = 12566;
+const APP_KEY = "5147cca9a4696edf";
 export default class App extends React.Component {
+
+ constructor(){
+    super();
+    this.state = {
+        zipcode : ZIP_CODE,
+        days : []
+    }
+  }
+  _getForecast(zipcode){
+
+    const weather_url = "http://api.wunderground.com/api/" + APP_KEY + "/forecast/q/" + zipcode +".json";
+
+    axios.get(weather_url).then( (response)=> {
+        if(response.status == 200){
+           /* var weather = response.data.forecast.simpleforecast.forecastday;
+            var forecast = [];*/
+            console.log(response.data);
+        }
+
+    });
+  }
   render() {
+    if(this.state.days.length<=0){
+        this._getForecast(this.state.zipcode)
+
+    }
     return (
       <View style={styles.container}>
 
