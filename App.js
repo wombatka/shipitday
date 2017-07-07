c7a20d0d6b9b1aba96c7e55b06e400a8183c9755
magda <midzik@consol.pl>
2017-07-07 11:20:22 +0200

    Display data from response

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import axios from 'axios';
+import _ from 'lodash';
+
+
 const ZIP_CODE = 12566;
 const APP_KEY = "5147cca9a4696edf";
 export default class App extends React.Component {
@@ -9,8 +12,11 @@ export default class App extends React.Component {
     super();
     this.state = {
         zipcode : ZIP_CODE,
-        days : []
+        days : [],
+        forecast: {}
     }
+    this._getForecast = this._getForecast.bind(this)
+
   }
   _getForecast(zipcode){
 
@@ -21,6 +27,7 @@ export default class App extends React.Component {
            /* var weather = response.data.forecast.simpleforecast.forecastday;
             var forecast = [];*/
             console.log(response.data);
+            this.setState({forecast: response.data});
         }
 
     });
@@ -32,7 +39,12 @@ export default class App extends React.Component {
     }
     return (
       <View style={styles.container}>
-
+        {/* <Text>{JSON.stringify(this.state.forecast, null, 2)}</Text> */}
+        <Text style={styles.temperature}>{_.get(this.state.forecast, 'forecast.simpleforecast.forecastday[0].high.celsius')} { "stopni xd " }</Text>
+        <Image
+          style={{width: 100, height: 100}}
+          source={{uri: _.get(this.state.forecast, 'forecast.simpleforecast.forecastday[0].icon_url')}}
+        />
       </View>
     );
   }
@@ -45,4 +57,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  temperature: {
+    color: '#ffffff',
+    fontSize: 30,
+
+  }
 });
