04524f5ed208eb2bcd8e3167cad4dbc9bba54330
Joanna Niechwiedowicz <joannan@consol.de>
2017-07-07 09:40:30 +0200

    git ignore + background + axios

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import axios from 'axios'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+
       </View>
     );
   }
@@ -16,7 +15,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3b5998',
     alignItems: 'center',
     justifyContent: 'center',
   },
